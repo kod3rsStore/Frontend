9b4f9e6c3a94fff9c9a9ca1dce89089cb907bea8
andresalba <tensinkusum@hotmail.com>
2020-05-30 17:43:27 -0500

    Burger menu

--- src/Frontend/src/Components/Header.js
+++ src/Frontend/src/Components/Header.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../Styles/Header.css';
+import Modal from './Modal/Modal';
 
-function Header() {
-  return (
+class Header extends Component {
+
+  constructor() {
+      super();
+
+      this.state = {
+          isShowing: false
+      }
+  }
+
+  openModalHandler = () => {
+      this.setState({
+          isShowing: true
+      });
+  }
+
+  closeModalHandler = () => {
+      this.setState({
+          isShowing: false
+      });
+  }
+
+  render () {
+      return (
     <div className="Headr">
-        <div className="Headr__item1"><a href="/menu1" target="_self"><img src="/img/menuLgt.png" className="iconMenu" alt="twitter" /></a></div>
+        <div className="Headr__cont1">
+          { this.state.isShowing ? <div onClick={this.closeModalHandler} className="back-drop"></div> : null }
+          <button className="open-modal-btn" onClick={this.openModalHandler}></button>
+          <div className="Headr__item1">
+
+            <Modal className="modal"
+                      show={this.state.isShowing}
+                      close={this.closeModalHandler}>
+            </Modal>
+          </div>
+        </div>
         <div className="Headr__item2"><img id="logoKod3rs" src="/img/logo1.png" alt=""/></div>
         <a href="https://google.com" className="Headr__item3">Home</a>
         <a href="https://google.com" className="Headr__item3">Login</a>
@@ -19,6 +52,7 @@ function Header() {
         </div>
     </div>
   );
+}
 }
 
 export default Header;
